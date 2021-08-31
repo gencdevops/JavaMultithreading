c6e0426005eaa69186ba841f700214b2fd7c1d65
gencdevops <gencdevops@gmail.com>
2021-08-31 13:25:37 +0300

    ProducerConsumerWaitNotifyApplication Spring initialize

---
+++ ProducerConsumerWaitNotify/src/main/java/com/example/producerconsumerwaitnotify/ProducerConsumerWaitNotifyApplication.java
@@ -0,0 +1,13 @@
+package com.example.producerconsumerwaitnotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerConsumerWaitNotifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProducerConsumerWaitNotifyApplication.class, args);
+    }
+
+}
---
+++ ProducerConsumerWaitNotify/src/test/java/com/example/producerconsumerwaitnotify/ProducerConsumerWaitNotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.producerconsumerwaitnotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerConsumerWaitNotifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
